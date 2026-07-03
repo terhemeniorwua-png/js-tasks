912667d5777de80da2f3742ff41ad0fd7154ed6c
PHILIP <terhemeniorwua@gmail.com>
2026-07-03 19:08:53 +0100

    add a condition for withdrawal

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-
-
 class BankAccount{
     constructor(accName, amount){
         this.name  = accName
         this.accuNum = 0
+        this.accBalance = amount
+        this.freeze =false
         this.acumNumber()
-        this.accBalance = 0
         this.withdraw(amount)
         this.deposit(amount)
+        // this.freeze()
+        
     }
     acumNumber(){
-       this.accuNum += (Math.floor(Math.random() * 100000000000)) 
+       this.accuNum += (Math.floor(Math.random() * 1000000000)) 
     }
     withdraw(amount){
-       this.accBalance -= amount
+       if(this.accBalance >= amount){
+        this.accBalance -= amount
+       } else{
+        this.Info = `Transaction declined. Insufficient account balance.`
+       }
     }
      deposit(amount){
-         this.accBalance += amount
+        this.accBalance += amount
+    }
+    freeze(){
+        this.freeze = true
     }
 }
 
 
-let user1 = new BankAccount('phil', 0)
-user1.deposit(5000)
-user1.withdraw(3900)
+let user1 = new BankAccount('phil', 2000)
+user1.deposit(1000)
+user1.withdraw(4000)
+// user1.freeze()
 console.log(user1)
